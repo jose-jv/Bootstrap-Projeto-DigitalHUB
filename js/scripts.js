6ffc11569cd5ccd258e38f4cd4c36e3c7ff6f8ae
José Vitor Tostes De Andrade <jvta.jose@gmail.com>
2023-12-29 09:20:56 -0300

    parallax
    utilizando parallax

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,142 @@
-const carousel = new bootstrap.Carousel('#myCarousel')
+
+//vai carregar so quando doc estiver pronto
+$(document).ready(function(){
+   
+    //progress bar
+    let containerA = document.getElementById("circleA");
+    
+    let circleA = new ProgressBar.Circle(containerA,{
+       color: '#FF0000',
+       strokeWidth:8 ,
+       duration:1400,
+       from: {color:'#AAA'},
+       to:{color: '#FF0000'},
+
+       step: function( state, circle){
+         circle.path.setAttribute('stroke',state.color);
+
+          let value = Math.round(circle.value()* 60);
+
+
+          circle.setText(value);
+       },
+    });
+
+     
+
+
+
+
+
+     let containerB = document.getElementById("circleB");
+    
+    let circleB = new ProgressBar.Circle(containerB,{
+       color: '#FF0000',
+       strokeWidth:8 ,
+       duration:1600,
+       from: {color:'#AAA'},
+       to:{color: '#FF0000'},
+
+       step: function( state, circle){
+         circle.path.setAttribute('stroke',state.color);
+
+          let value = Math.round(circle.value()* 120);
+
+
+          circle.setText(value);
+       },
+    });
+
+    
+
+
+
+     
+     let containerC = document.getElementById("circleC");
+    
+    let circleC= new ProgressBar.Circle(containerC,{
+       color: '#FF0000',
+       strokeWidth:8 ,
+       duration:1800,
+       from: {color:'#AAA'},
+       to:{color: '#FF0000'},
+
+       step: function( state, circle){
+         circle.path.setAttribute('stroke',state.color);
+
+          let value = Math.round(circle.value()* 90);
+
+
+          circle.setText(value);
+       },
+    });
+    
+
+
+
+
+
+
+
+
+
+     
+     let containerD = document.getElementById("circleD");
+    
+    let circleD = new ProgressBar.Circle(containerD,{
+       color: '#FF0000',
+       strokeWidth:8 ,
+       duration:2000,
+       from: {color:'#AAA'},
+       to:{color: '#FF0000'},
+
+       step: function( state, circle){
+         circle.path.setAttribute('stroke',state.color);
+
+          let value = Math.round(circle.value()* 130);
+
+
+          circle.setText(value);
+       },
+    });
+
+          //iniciando o loader quando o usuario chega no elemento
+     let dataAreaOffset = $('#data-area').offset();
+     let stop = 0;
+
+     $(window).scroll(function(e){
+        let scroll = $(window).scrollTop();
+
+        if(scroll > (dataAreaOffset.top -500)&& stop == 0){
+             circleA.animate(1,9);
+             circleB.animate(1,8);
+             circleC.animate(1,6);
+             circleD.animate(1,2);
+
+             stop =1;
+        }
+     });
+
+
+
+
+     //parallax
+     setTimeout(function(){
+        $('#data-area').parallax({imageSrc:'img/imgparallax.jpg'});
+
+     }, 250);
+
+   
+
+
+
+
+
+
+
+
+
+
+
+    
+});
