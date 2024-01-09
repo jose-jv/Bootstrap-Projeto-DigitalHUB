ba84014fe160c42d65bec639372dd85cf156427a
José Vitor Tostes De Andrade <jvta.jose@gmail.com>
2024-01-09 09:14:34 -0300

    scroll navbar
    scroll navbar

--- js/scripts.js
+++ js/scripts.js
@@ -175,6 +175,54 @@ function eachBoxes(type,boxes){
 }
 
 
+//scroll para secoes
+let navBtn = $('.nav-item');
+
+let homeSection = $("#carouselExampleCaptions")
+let bannerSection = $("#mainSlider");
+let aboutSection = $("#about-area");
+let servicesSection = $("#services-area");
+let teamSection = $("#team-area");
+let portfolioSection = $("#portfolio-area");
+let contactSection = $("#contact-area");
+
+let scrolTo = '';
+
+$(navBtn).click(function(){
+   let btnId = $(this).attr('id');
+   console.log(btnId);
+
+    if(btnId == 'home-menu'){
+      scrollTo = carouselExampleCaptions;
+    }
+    else if(btnId == 'about-menu'){
+      scrollTo = aboutSection;
+   }else if(btnId == 'services-menu'){
+      scrollTo = servicesSection;}
+      else if (btnId == 'team-menu') {
+         scrollTo = teamSection;
+       }
+       else if(btnId == 'portfolio-menu'){
+      scrollTo = portfolioSection;
+   }else if(btnId == 'contact-menu'){
+      scrollTo = contactSection;
+   }else if(btnId == 'portfolio-menu'){
+      scrollTo = portfolioSection;
+   } else {
+      scrollTo = bannerSection;
+   }
+
+
+$([document.documentElement, document.body]).animate({
+   scrollTop: $(scrollTo).offset().top - 70
+}, 1500);
+
+});
+
+
+
+
+
 
 
 
